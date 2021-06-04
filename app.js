5e64a7d060d201355d99866ec9e34dd37a4a174d
Mr-Nelson <kdnels88@gmail.com>
2021-06-04 15:33:25 -0500

    added Javascript function to correct user inputs to match data.JS

--- app.js
+++ app.js
@@ -1,72 +1,90 @@
 'use strict';
 
-function searchByName(){
-    // Grabbing the values from our nameForm form and inputs.
-    let firstNameInput = document.forms['nameForm']['fname'].value;
-    let lastNameInput = document.forms['nameForm']['lname'].value;
+// function searchByName(){
+//     // Grabbing the values from our nameForm form and inputs.
+//     let firstNameInput = document.forms['nameForm']['fname'].value;
+//     let lastNameInput = document.forms['nameForm']['lname'].value;
 
-    // "people" is coming from the data.js file. We have access to it within this JavaScript file.
-    let filteredPeople = people.filter(function (person) {
-        if(person.firstName === firstNameInput && person.lastName === lastNameInput){
-            return true;
-        }
-        return false;
-    });
+//     // "people" is coming from the data.js file. We have access to it within this JavaScript file.
+//     let filteredPeople = people.filter(function (person) {
+//         if(person.firstName === firstNameInput && person.lastName === lastNameInput){
+//             return true;
+//         }
+//         return false;
+//     });
     
-    // Rather than console logging, you need to append the filteredPeople to a table.
-    if(filteredPeople.length > 0){
-        console.log(filteredPeople);
-    }else{
-        console.log('Sorry, looks like there is no one with that name.');
-    }
-}
+//     // Rather than console logging, you need to append the filteredPeople to a table.
+//     if(filteredPeople.length > 0){
+//         console.log(filteredPeople);
+//     }else{
+//         console.log('Sorry, looks like there is no one with that name.');
+//     }
+// }
 
-people.map(function(el)){
-    document.getElementById("people").innerHTML += `<tr>
-    <td>${el.firstName}</td>
-    <td>${el.lastName}</td>
-    <td>${el.gender}</td>
-    <td>${el.dob}</td>
-    <td>${el.height}</td>
-    <td>${el.weight}</td>
-    <td>${el.eyeColor}</td>
-    <td>${el.occupation}</td>
-    <td>${el.parents}</td>
-    <td>${el.currentSpouse}</td>
-    </tr>`
-}
+let userInput= correctUserInput(prompt("Enter string here."));
 
-function filterPeople(){
-    people.filter(function(el)){
-        document.getElementById("people").innerHTML == `<tr>
-        <td>${el.firstName}</td>
-        <td>${el.lastName}</td>
-        <td>${el.gender}</td>
-        <td>${el.dob}</td>
-        <td>${el.height}</td>
-        <td>${el.weight}</td>
-        <td>${el.eyeColor}</td>
-        <td>${el.occupation}</td>
-        <td>${el.parents}</td>
-        <td>${el.currentSpouse}</td>
-        </tr>`
-    }
+function correctUserInput(string){
+    let correctedInput =[];
+        for (let i = 0; i < string.length; i++){
+            if (i != 0){
+                string[i].toString();
+                correctedInput += string[i].toLowerCase();
+            }
+            else if(i == 0){
+                string[i].toString();
+                correctedInput += string[i].toUpperCase();
+            }
+        }
+    return correctedInput;
 }
-function descendants(){
-    people.filter(function(el)){
-        document.getElementById("people").innerHTML == `<tr>
-        <td>${el.firstName}</td>
-        <td>${el.lastName}</td>
-        </tr>`
-    }
-}
-function immediateFamily(){
-    people.filter(function(el)){
-        document.getElementById("people").innerHTML == `<tr>
-        <td>${el.firstName}</td>
-        <td>${el.lastName}</td>
-        <td>${el.parents}</td>
-        <td>${el.currentSpouse}</td>
-        </tr>`
-    }
-}
+console.log(userInput);
+
+// people.map(function(el)){
+//     document.getElementById("people").innerHTML += `<tr>
+//     <td>${el.firstName}</td>
+//     <td>${el.lastName}</td>
+//     <td>${el.gender}</td>
+//     <td>${el.dob}</td>
+//     <td>${el.height}</td>
+//     <td>${el.weight}</td>
+//     <td>${el.eyeColor}</td>
+//     <td>${el.occupation}</td>
+//     <td>${el.parents}</td>
+//     <td>${el.currentSpouse}</td>
+//     </tr>`
+// }
+
+// function filterPeople(){
+//     people.filter(function(el)){
+//         document.getElementById("people").innerHTML == `<tr>
+//         <td>${el.firstName}</td>
+//         <td>${el.lastName}</td>
+//         <td>${el.gender}</td>
+//         <td>${el.dob}</td>
+//         <td>${el.height}</td>
+//         <td>${el.weight}</td>
+//         <td>${el.eyeColor}</td>
+//         <td>${el.occupation}</td>
+//         <td>${el.parents}</td>
+//         <td>${el.currentSpouse}</td>
+//         </tr>`
+//     }
+// }
+// function descendants(){
+//     people.filter(function(el)){
+//         document.getElementById("people").innerHTML == `<tr>
+//         <td>${el.firstName}</td>
+//         <td>${el.lastName}</td>
+//         </tr>`
+//     }
+// }
+// function immediateFamily(){
+//     people.filter(function(el)){
+//         document.getElementById("people").innerHTML == `<tr>
+//         <td>${el.firstName}</td>
+//         <td>${el.lastName}</td>
+//         <td>${el.parents}</td>
+//         <td>${el.currentSpouse}</td>
+//         </tr>`
+//     }
+// }
