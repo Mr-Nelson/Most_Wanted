43f3e9c4a12eb4487141299e9031bcb1f05191e4
Mr-Nelson <kdnels88@gmail.com>
2021-06-09 15:20:49 -0500

    cleaned up JavaScript code

--- app.js
+++ app.js
@@ -1,27 +1,4 @@
 'use strict';
-
-// function searchByName(){
-//     // Grabbing the values from our nameForm form and inputs.
-//     let firstNameInput = document.forms['nameForm']['fname'].value;
-//     let lastNameInput = document.forms['nameForm']['lname'].value;
-
-//     // "people" is coming from the data.js file. We have access to it within this JavaScript file.
-//     let filteredPeople = people.filter(function (person) {
-//         if(person.firstName === firstNameInput && person.lastName === lastNameInput){
-//             return true;
-//         }
-//         return false;
-//     });
-    
-//     // Rather than console logging, you need to append the filteredPeople to a table.
-//     if(filteredPeople.length > 0){
-//         console.log(filteredPeople);
-//     }else{
-//         console.log('Sorry, looks like there is no one with that name.');
-//     }
-// }
-
-
 // function correctUserInput(string){
 //     let correctedInput =[];
 //         for (let i = 0; i < string.length; i++){
@@ -36,7 +13,6 @@
 //         }
 //     return correctedInput;
 // }
-
 
 function buildTable(){
     people.map(function(el) {
@@ -59,29 +35,8 @@ function resetTable() {
   })
 }  
 
-// let results = people;
-// results = searchByEyeColor('blue', results)
-// results = searchByGender('male', results)
-// console.log(results)
-
-
-
-// function searchByGender(gender, peopleToSearch){
-//     let filteredPeople = peopleToSearch.filter(function(person){
-//         if(banana.gender == gender){
-//             return true;
-//         }
-//         else{
-//             return false;
-//         }
-//     })
-//     return filteredPeople
-// }
-
-
-
 function filterPeople (){
-    let userFormInputs = [  //[Joy,"","","","","","",""]
+    let userFormInputs = [ 
     document.forms["form"]["fname"].value,
     document.forms["form"]["lname"].value,
     document.forms["form"]["gender"].value,
@@ -277,10 +232,6 @@ function searchForDescendants(person, object){
 //     }
 // }
 
-
-
-
-
 //  function finalSearch(){
 //      let finalSearchOutput = people;
 
@@ -302,7 +253,6 @@ function searchForDescendants(person, object){
 
 //          return finalSearchOutput;
 //  }
-
 
 // function searchByParents(person) {
 //     let i = 0;
@@ -354,119 +304,3 @@ function searchForDescendants(person, object){
 //         return;
 //     }
 // }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//     let peopleSearch = people.filter(function(characteristic){     //[Joy,"","","","","","",""]
-//         if (userFormInputs[0] === characteristic.firstName || userFor == people.lastName || people.gender || people.dob || people.height || people.weight || people.eyeColor || people.occupation){  //BOOM
-//             return true;
-//         }
-//         else{
-//             return false;
-//         }
-//     document.getElementById("people").innerHTML == `<tr>
-//         <td>${el.firstName}</td>
-//         <td>${el.lastName}</td>
-//         <td>${el.gender}</td>
-//         <td>${el.dob}</td>
-//         <td>${el.height}</td>
-//         <td>${el.weight}</td>
-//         <td>${el.eyeColor}</td>
-//         <td>${el.occupation}</td>
-//         <td>${el.parents}</td>
-//         <td>${el.currentSpouse}</td>
-//         </tr>`;
-//     })
-// }
-//     filterPeople(userFormInputs.id == people.parents);
-//          if (true)
-//              filterPeople(people.parents == people.id)
-//                  if (true){
-//                      document.getElementById("people").innerHTML == `<tr>
-//                          <td>${el.firstName}</td>
-//                          <td>${el.lastName}</td>
-//                          <td>${el.gender}</td>
-//                          <td>${el.dob}</td>
-//                          <td>${el.height}</td>
-//                          <td>${el.weight}</td>
-//                          <td>${el.eyeColor}</td>
-//                          <td>${el.occupation}</td>
-//                          <td>${el.parents}</td>
-//                          <td>${el.currentSpouse}</td>
-//                          </tr>`
-//                          }
-//          else if (true){
-//              document.getElementById("people").innerHTML == `<tr>
-//                     <td>${el.firstName}</td>
-//                     <td>${el.lastName}</td>
-//                     <td>${el.gender}</td>
-//                     <td>${el.dob}</td>
-//                     <td>${el.height}</td>
-//                     <td>${el.weight}</td>
-//                     <td>${el.eyeColor}</td>
-//                     <td>${el.occupation}</td>
-//                     <td>${el.parents}</td>
-//                     <td>${el.currentSpouse}</td>
-//                     </tr>`
-//                     }
-// }
-
-// descendantsButton(){
-//  if ( == true) {
-//    function filterPeople(userFormInputs.id == people.parents){
-//      people.filter(function(el){
-//          document.getElementById("people").innerHTML == `<tr>
-//          <td>${el.firstName}</td>
-//          <td>${el.lastName}</td>
-//          <td>${el.gender}</td>
-//          <td>${el.dob}</td>
-//          <td>${el.height}</td>
-//          <td>${el.weight}</td>
-//          <td>${el.eyeColor}</td>
-//          <td>${el.occupation}</td>
-//          <td>${el.parents}</td>
-//          <td>${el.currentSpouse}</td>
-//          </tr>`
-//          })
-//       }
-//     }
-//  }
-
-
-//immediateFamilyButton(){
-//  if () == true{
-//      function filterPeople(userFormInputs.parents, userFormInputs.currentSpouse,userFormInputs.id == people.parents, userFormInputs.parents == people.parents){
-//          people.filter(function(el)){
-//              document.getElementById("people").innerHTML == `<tr>
-//              <td>${el.firstName}</td>
-//              <td>${el.lastName}</td>
-//              <td>${el.parents}</td>
-//              <td>${el.currentSpouse}</td>
-//              </tr>`
-//              })
-//           }
-//      }
-//  }
-//}
-
-
-
